aadd07751b4f3a60cfa3311860e3cad4dc42d060
Phil Tsarik <phil.tsarik@gmail.com>
2016-01-28 22:57:21 +0100

    Return lastfm_sql.cmd script back to the repo
    This script is used while importing Last.fm playcount data.

---
+++ lastfm_sql.cmd
@@ -0,0 +1,12 @@
+@echo off
+color 97
+cls
+echo ***************IMPORTANT***************
+echo ***************************************
+echo YOU MUST NOW CLOSE FOOBAR MANUALLY 
+echo BEFORE YOU...
+pause
+cls
+echo working....
+%1 %2 <%3
+start foobar2000
